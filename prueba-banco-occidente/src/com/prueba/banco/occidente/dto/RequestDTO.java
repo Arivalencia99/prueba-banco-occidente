61ef049e28c8940fe6b9a1cbfdaa6bacdc1b9407
arivalencia <ari_valencia@gtccorporation.com>
2023-08-29 22:40:02 -0500

    proyecto

---
+++ prueba-banco-occidente/src/com/prueba/banco/occidente/dto/RequestDTO.java
@@ -0,0 +1,15 @@
+package com.prueba.banco.occidente.dto;
+
+public class RequestDTO {
+	
+	private Integer groups;
+
+	public Integer getGroups() {
+		return groups;
+	}
+
+	public void setGroups(Integer groups) {
+		this.groups = groups;
+	}
+
+}
---
+++ prueba-banco-occidente/src/com/prueba/banco/occidente/dto/ResponseDTO.java
@@ -0,0 +1,7 @@
+package com.prueba.banco.occidente.dto;
+
+public class ResponseDTO {
+	
+	private String sizes;
+	
+}
